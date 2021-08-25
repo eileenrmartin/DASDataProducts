1c8ee1dbf903375f427a32d76e59ff54a4b938c5
Brandon Pearl <branp923@vt.edu>
2021-08-25 18:55:15 -0400

    still working on downsampling method

--- Examples/lowpassEx.py
+++ Examples/lowpassEx.py
@@ -8,16 +8,16 @@ from scipy.signal import iirfilter, zpk2sos, sosfilt
 import warnings
 plt.switch_backend('agg')
 
-cutoffFrequency = 80;
-filterOrder = 4;
+filterOrder = 6;
+integerDownsampleFactor = 10;
 #Run lowpass filter
-(time,signal,filtered_signal, num_samples, sample_freq) = lowpass.runLowpass(cutoffFrequency,filterOrder);
-
+(time,signal,filtered_signal,num_samples,sample_duration,sample_freq) = lowpass.runLowpass(integerDownsampleFactor,filterOrder);
+(downsample_time, downsample_signal) = lowpass.runDownsample(integerDownsampleFactor,sample_duration,filtered_signal);
 channelNumber = 50;
 startTime = 0.10;
 endTime = 0.15;
 
 #Plot lowpass filter on channelNumber
-lowpass.plotLowpass(time,signal,filtered_signal,channelNumber,startTime,endTime);
+lowpass.plotLowpass(time,signal,filtered_signal,downsample_time,downsample_signal,channelNumber,startTime,endTime);
 lowpass.plotAmplitudeSpectrum(signal, filtered_signal, channelNumber, num_samples, sample_freq);
 
--- SourceCode/lowpass.py
+++ SourceCode/lowpass.py
@@ -3,25 +3,41 @@ import matplotlib.pyplot as plt
 import scipy.signal
 from T15 import server_func
 import condenser
-from scipy.signal import iirfilter, zpk2sos, sosfilt
+from scipy.signal import iirfilter, zpk2sos, sosfilt, decimate
 import warnings
 from scipy.fft import rfft, rfftfreq
 import sys
 plt.switch_backend('agg')
 
-def runLowpass(cutoffFrequency, filterOrder):
+
+def runLowpass(integerDownsampleFactor, filterOrder):
     
     client = server_func.setup_server();
     data, md = server_func.get_data(client,1);
-    cutoff_freq = cutoffFrequency;
+    #cutoff_freq = cutoffFrequency;
     dT = md['dT'];
     sampling_freq = 1/dT;
+    nyquist_freq = sampling_freq/2;
+    cutoff_freq = (int) (nyquist_freq/integerDownsampleFactor);
+    #print(cutoff_freq, file=sys.stderr);
     data_T = np.transpose(data);
     lowpassData = lowpass(data_T, cutoff_freq, sampling_freq, filterOrder, zerophase=True);
     number_of_time_samples = md['nT'];
     sampling_duration = number_of_time_samples * dT;
-    time = np.linspace(0, sampling_duration, number_of_time_samples, endpoint=False)
-    return (time, data_T, lowpassData, number_of_time_samples, sampling_freq)
+    time = np.linspace(0, sampling_duration, number_of_time_samples, endpoint=False);
+    #downsampled_data = lowpassData[::integerDownsampleFactor);
+    #downsampled_data = scipy.signal.decimate(lowpassData,integerDownsampleFactor,n=filterOrder);
+    print(cutoff_freq, file=sys.stderr);
+    return (time, data_T, lowpassData,number_of_time_samples,sampling_duration,sampling_freq)
+
+
+def runDownsample(integerDownsampleFactor,samplingDuration,lowpassData):
+    downsampled_data = scipy.signal.decimate(lowpassData,integerDownsampleFactor);
+    print(len(downsampled_data[0]), file=sys.stderr);
+    newNumSamples = len(downsampled_data[0]);
+    downsampled_time = np.linspace(0,samplingDuration,newNumSamples, endpoint=False);
+    print(len(downsampled_time), file=sys.stderr);
+    return (downsampled_time, downsampled_data)
 
 def plotAmplitudeSpectrum(signal,filteredSignal,channelNumber,numSamples, signalFreq):
     signal = np.transpose(signal);
@@ -53,14 +69,21 @@ def plotAmplitudeSpectrum(signal,filteredSignal,channelNumber,numSamples, signal
 
 
 
-def plotLowpass(time,signal,filteredSignal,channelNumber,startTime,endTime):
+def plotLowpass(time,signal,filteredSignal,downsampleTime,downsampledSignal,channelNumber,startTime,endTime):
     temp = np.where(time >= startTime);
     first = min(min(temp));
     temp = np.where(time <= endTime);
     last = max(max(temp));
+    
+    temp = np.where(downsampleTime >= startTime);
+    first_d = min(min(temp));
+    temp = np.where(downsampleTime <= endTime);
+    last_d = max(max(temp));
+
     fig = plt.figure(figsize=(10,10));
     plt.plot(time[first:last], signal[first:last,channelNumber], 'b-', label='signal')
     plt.plot(time[first:last], filteredSignal[first:last, channelNumber], 'g-', label='filtered signal')
+    plt.plot(downsampleTime[first_d:last_d], downsampledSignal[first_d:last_d, channelNumber], 'r-', label='downsampled signal')
     plt.xlabel("Time (s)");
     plt.ylabel("Amplitude");
     plt.legend();
