3f6bbdca405313b77de93705b27ad9c550439708
sampaulus <49253049+sampaulus@users.noreply.github.com>
2021-10-18 09:43:44 -0400

    Add files via upload

--- SourceCode/save_data_prod.py
+++ SourceCode/save_data_prod.py
@@ -9,6 +9,7 @@ and calculate and store data products in a file.
 
 from T15 import server_func
 import sys
+import lowpassDownsample
 
 if __name__ == '__main__':
     print("Fetch data from server attempt")
@@ -17,3 +18,18 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     
     server_func.save_file(filename)
+    #(time,signal,downsample_time, downsample_signal,sample_freq) = lowpassDownsample.runLowpassAndDownsample(2)
+    
+    #channelNumber = 50;
+    #startTime = 0;
+    #endTime = 0.15;
+    #print(signal.shape)
+    #print(time.shape)
+    #import numpy as np 
+    #signal = np.transpose(signal)
+    #print(downsample_signal.shape)
+    #print(downsample_time.shape)
+    #downsample_signal = np.transpose(downsample_signal)
+    #Plot lowpass filter on channelNumber
+    #lowpassDownsample.plotLowpass(time,signal,downsample_time,downsample_signal,channelNumber,startTime,endTime);
+    #lowpassDownsample.plotAmplitudeSpectrum(signal, downsample_signal, channelNumber, sample_freq, sample_freq);
