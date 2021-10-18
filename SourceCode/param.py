04ea13d0c09e61a4ead6e8b121ab33f432d45828
sampaulus <49253049+sampaulus@users.noreply.github.com>
2021-10-18 09:27:15 -0400

    Add files via upload

--- SourceCode/param.py
+++ SourceCode/param.py
@@ -2,7 +2,9 @@
 # line 6 is ch_group_size, the number of channels in each channel group (int),
 # line 7 is min_data, the number of minutes of data to fetch (int), should be same interval as in the crontab file,  
 # line 8 is file_path, the path to directory to save hdf data product files (string)
+# line 9 is the downsampling factor to use in downsampling data to decimate original signal
 
-ch_group_size = 100
+ch_group_size = 10
 min_data = 1
-file_path = "."
+file_path = "./files"
+downsamp_factor = 2
