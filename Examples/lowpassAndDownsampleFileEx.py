5b2c80daa3e1dc1aad26e0a79d3a466e2f95250a
branp923 <branp923@vt.edu>
2021-11-12 11:47:56 -0500

    fixed lowpass and downsample

--- Examples/lowpassAndDownsampleFileEx.py
+++ Examples/lowpassAndDownsampleFileEx.py
@@ -12,17 +12,20 @@ plt.switch_backend('agg')
 
 def main(argv):
 	fileName = argv[0]
-	integerDownsampleFactor = 8;
+	integerDownsampleFactor = 10;
 	#Get data
 	(data, sampling_duration, number_time_samples, sampling_freq) = lowpassDownsample.getFileData(fileName);
 	#Run lowpass filter and downsample 
-	(time, signal, downsample_time, downsample_signal, sample_freq, downsampled_sample_freq) = lowpassDownsample.runLowpassAndDownsample(data, sampling_duration, number_time_samples,sampling_freq,integerDownsampleFactor);
-	channelNumber = 25;
-	startTime = 3.00;
-	endTime = 10.0;
+	(time, signal, downsample_time, downsample_signal, sample_freq, downsampled_sample_freq) = lowpassDownsample.runLowpassAndDownsample(data,sampling_duration,number_time_samples,sampling_freq,integerDownsampleFactor);
+	channelNumber = 30;
+	#print(downsample_time.shape, file = sys.stderr);
+	print(downsample_signal[channelNumber,:].shape, file = sys.stderr);
 	#Plot lowpass filter on channelNumber
-	lowpassDownsample.plotLowpassDownsample(time,signal,downsample_time,downsample_signal,channelNumber,startTime,endTime);
+	lowpassDownsample.plotLowpassDownsample(time,signal,downsample_time,downsample_signal,channelNumber);
+	
+	#lowpassDownsample.plotLowpass(time,signal,channelNumber);
 	lowpassDownsample.plotAmplitudeSpectrum(signal, downsample_signal, channelNumber, sample_freq,downsampled_sample_freq);
+	#lowpassDownsample.plotDownsample(downsample_time,downsample_signal,channelNumber);
 if __name__ == "__main__":
 	main(sys.argv[1:])
 
