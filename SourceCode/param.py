60031cc563f792917cae2ef2f7af009e6ef0bf8e
sampaulus <49253049+sampaulus@users.noreply.github.com>
2022-03-17 17:28:57 -0400

    Update param.py

--- SourceCode/param.py
+++ SourceCode/param.py
@@ -1,5 +1,5 @@
 # parameter file setup for save_data_prod.py: (any parameter files used should have .py extension and have the same parameter names)
-# line 6 is ch_group_size, the number of channels in each channel group (int),
+# line 6 is ch_group_size, the number of channels in each channel group that will be averaged together (int),
 # line 7 is min_data, the number of minutes of data to fetch (int), should be same interval as in the crontab file,  
 # line 8 is file_path, the path to directory to save hdf data product files (string)
 # line 9 is the downsampling factor to use in downsampling data to decimate original signal
